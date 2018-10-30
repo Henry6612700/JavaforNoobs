35158e64a13a3d2dcff3fb1957c34699ae1f0357
Henry <34593843+Henry6612700@users.noreply.github.com>
2018-10-30 16:56:07 +0700

    ok

--- test1.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	int a,*b;
-	a=5;
-	b=&a;
-	cout<<"Pointer b menyimpan alamat : "<<b<<endl;
-	cout<<"Isi (nilai) dari alamat tersebut : "<<*b<<endl;
-}
