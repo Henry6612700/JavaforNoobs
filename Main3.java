67686205509a56b725c149321547d4a15c5c281b
Henry6612700 <henryciang451@gmail.com>
2018-11-03 11:12:32 +0700

    Add file string concatenation & Escape Characters

---
+++ Main3.java
@@ -0,0 +1,10 @@
+public class Main3{
+	public static void main(String[]args){ 
+		System.out.println("Hello World");
+		String firstName="Pedro";
+		String lastName="Mercado";
+		String fullName= firstName + " " +lastName+123;
+		System.out.println(fullName);
+		System.out.println("\tPedro\n said\\, \n\"I love chicken\"");
+	}
+}
