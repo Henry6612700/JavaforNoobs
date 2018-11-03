d4a9d060171f7c33a8936e6ded07130adfb33979
Henry6612700 <henryciang451@gmail.com>
2018-11-03 10:13:36 +0700

    Add file type data primitives and variables

---
+++ Main1.java
@@ -0,0 +1,15 @@
+public class Main1{
+	public static void main(String[]args){
+		//byte 2^8-128 -> 127
+		//short 2^16-32,768 -> 32,767
+		//int -2^31 -> 2^31-1
+		//long -2^63 -> 2^63-1
+		//short number1=10;	
+		double x=3.14;
+		System.out.println(x);
+		boolean trueOfFalse = false;
+		System.out.println(trueOfFalse);
+		char someCharacter ='H';
+		System.out.println(someCharacter);
+	}
+}
