dc4d812817b1005a89fa0ac5dbe1ee66a601a8ad
Henry6612700 <henryciang451@gmail.com>
2018-11-05 07:44:02 +0700

    Add file Arithmetic Operator

---
+++ Main4.java
@@ -0,0 +1,16 @@
+public class Main5{
+	public static void main(String[]args){
+		double num1=5;
+		double num2=7;
+		double result=num1+num2;
+		System.out.println(num1+" + " +num2+" = "+result);
+		result=num1-num2;
+		System.out.println(num1+" - " +num2+" = "+result);
+		result=num1*num2;
+		System.out.println(num1+" * " +num2+" = "+result);
+		result=num1/num2;
+		System.out.println(num1+" / " +num2+" = "+result);
+		result=num1%num2;
+		System.out.println(num1+" % " +num2+" = "+result);
+	}
+}
